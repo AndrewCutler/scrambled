6b581b292d9481fea2cd2d462173bda6146811ba
Andrew Cutler <Andrew.Cutler@fusionmgt.com>
2023-07-05 18:31:43 -0400

    got movement working by removing "viewOnly"

--- client/src/components/chess/board/Board.tsx
+++ client/src/components/chess/board/Board.tsx
@@ -1,20 +1,51 @@
-import { Component, onMount } from 'solid-js';
+import { Component, onCleanup, onMount } from 'solid-js';
 
+import { Api } from 'chessground/api';
 import { Chess } from 'chess.js';
 import { Chessground } from 'chessground';
 
+const startingFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const fen =
+	'rnb1k1r1/1p1p4/2p1p3/4Pn1p/p2P1P2/2P2N1q/PP1QB1p1/R3K1R1 b q - 11 23';
+
 const Board: Component<{}> = (props) => {
+	let cgApi: Api | undefined;
+	let board: Chess | undefined;
+
 	onMount(() => {
-		const board = new Chess();
+		board = new Chess(fen);
+	});
+
+	onCleanup(() => {
+		cgApi?.destroy();
 	});
 
 	const mount = (el: HTMLDivElement) => {
-		const x = Chessground(el, {
-			viewOnly: true,
-			fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-			// fen: 'rnb1k1r1/1p1p4/2p1p3/4Pn1p/p2P1P2/2P2N1q/PP1QB1p1/R3K1R1 b q - 11 23'
+		cgApi = Chessground(el, {
+			animation: { enabled: true, duration: 250 },
+			movable: {
+				free: true,
+				color: 'both',
+				events: {
+					after: (original, dest, meta) => {
+						console.log({ original, dest, meta });
+					}
+				}
+			},
+			draggable: {
+				enabled: true
+			},
+			drawable: {
+				enabled: true
+			},
+			selectable: {
+				enabled: true
+			},
+			events: {
+				move: console.log
+			},
+			fen
 		});
-		console.log({ x });
 	};
 
 	return <div ref={(el) => mount(el)} />;
